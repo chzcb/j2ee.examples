59023d1df34c0a3ed98de9c5c7d499a1d79e7f18
张昌彪 <changbiao.zhang@bhex.com>
2020-05-11 16:32:47 +0800

    例子

---
+++ dhp-examples/rpc-demo/src/test/java/org/dhp/examples/rpcdemo/RpcClientDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dhp.examples.rpcdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class RpcClientDemoApplicationTests {
+
+    int TOTAL = 50000;
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
